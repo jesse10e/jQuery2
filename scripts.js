81579c376a5045bb197809cce28eb4416194e58a
jesse10e <jesse10e@gmail.com>
2017-04-20 16:02:03 -0600

    initial commit

---
+++ scripts.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+    var listOf = []
+    $('#newTaskForm').hide();
+
+    var advanceTask = function(task) {
+        var modified = task.innerText.trim()
+        for (var i = 0; i < listOf.length; i++) {
+            if (listOf[i].task === modified) {
+                if (listOf[i].id === 'new') {
+                    listOf[i].id = 'inProgress'
+                } else if (listOf[i].id === 'inProgress') {
+                    listOf[i].id = 'archived';
+                } else {
+                    listOf.splice(i, 1);
+                }
+                break;
+            }
+        }
+        task.remove()
+    };
+    var Task = function(task) {
+        this.task = task;
+        this.id = 'new';
+    };
+    var addTask = function(task) {
+        if (task) {
+            task = new Task(task);
+            listOf.push(task);
+
+            $('#newItemInput').val('');
+            $('#newList').append(
+                '<a href="#finish" class="" id="item">' +
+                '<li class="list-group-item">' +
+                '<h3>' + task.task + '</h3>' +
+                '<span class="arrow pull-right">' +
+                '<i class="glyphicon glyphicon-arrow-right">' +
+                '</span>' +
+                '</li>' +
+                '</a>'
+            );
+        }
+        $('#newTaskForm').slideToggle('fast', 'linear');
+    };
+    $('#saveNewItem').on('click', function(e) {
+        e.preventDefault();
+        var task = $('#newItemInput').val().trim();
+        addTask(task);
+    });
+    $('#add-todo').on('click', function() {
+        $('#newTaskForm').fadeToggle('fast', 'linear');
+    });
+    $('#cancel').on('click', function(e) {
+        e.preventDefault();
+        $('#newTaskForm').fadeToggle('fast', 'linear')
+    });
+    $(document).on('click', '#item', function(e) {
+        e.preventDefault();
+        var task = this;
+        advanceTask(task);
+        this.id = 'inProgress';
+        $('#currentList').append(this.outerHTML);
+    });
+    $(document).on('click', '#inProgress', function(e) {
+        e.preventDefault();
+        var task = this;
+        task.id = "archived";
+        var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
+        advanceTask(task);
+        $('#archivedList').append(changeIcon);
+    });
+    $(document).on('click', '#archived', function(e) {
+        e.preventDefault();
+        var task = this;
+        advanceTask(task);
+    })
+});
